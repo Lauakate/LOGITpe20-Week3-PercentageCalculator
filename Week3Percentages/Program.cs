c1c9c3a06edea040f5ffb276e62566ccb9763123
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:55:10 +0300

    Kalkulaator

---
+++ Week3Percentages/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Week3Percentages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Maximum;
+            int UserScore;
+            double Result;
+
+            Console.WriteLine("What is the maximum score?");
+            Maximum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What is your score?");
+            UserScore = Int32.Parse(Console.ReadLine());
+            Result = (UserScore * 100) / Maximum;
+
+            Console.WriteLine($"You scored {Result}%.");
+
+            if (Result >= 90)
+            {
+                Console.WriteLine("5");
+            }
+            else if (Result >= 80)
+            {
+                Console.WriteLine("4");
+            }
+            else if (Result >= 61)
+            {
+                Console.WriteLine("3");
+            }
+            else
+            {
+                Console.WriteLine("2");
+            }
+
+        }
+    }
+}
